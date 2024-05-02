1326cb8ffa7e2b3b4dcefd1592d0bf2b62102225
unknown <jishnurameasan619@gmail.com>
2024-05-02 17:09:08 +0530

    Modified the adminModel

--- models/adminModel.js
+++ models/adminModel.js
@@ -3,28 +3,21 @@ const mongoose = require('mongoose');
 const adminSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
-        trim: true,
-        lowercase: true
+        required: true, 
     },
     email: {
         type: String,
         required: true,
         unique: true,
-        trim: true,
-        lowercase: true
     },
     password: {
         type: String,
         required: true,
-        select: false,
-        trim: true
+        select: false, 
     },
     authorization_key: {
         type: String,
-        required: true,
-        trim: true,
-        lowercase: true
+        required: true,   
     }
 
 });
